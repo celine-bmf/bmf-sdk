61c66486cfd0c04eacd067374c9f9451395fabda
celine <celine@bemyfriends.com>
2025-08-11 11:25:48 +0900

    Release new XCFramework (checksum: 92b9b5b4e34d31ef9b7be8c9d008742b2f0a82c6c2355f53a7043f4d444bccb6)

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         .binaryTarget(
             name: "bmf-sdk",
             url: "https://github.com/celine-bmf/bmf-sdk/releases/download/1.0.3/bmf-sdk.xcframework.zip",
-            checksum: "fd6f16c9626930310d50fd810aacb1da7b4372d154752f3b5c3b58d125ae9ed9"
+            checksum: "92b9b5b4e34d31ef9b7be8c9d008742b2f0a82c6c2355f53a7043f4d444bccb6"
         )
     ]
 )
